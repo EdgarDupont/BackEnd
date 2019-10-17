223f0873e1a3c342f0d7800e3e12d3ae214232b9
EdgarDupont <55579204+EdgarDupont@users.noreply.github.com>
2019-10-17 10:23:58 +0200

    Update counter_test.rb

--- test/models/counter_test.rb
+++ test/models/counter_test.rb
@@ -1,1 +1,12 @@
+require 'test_helper'
 
+class CounterTest < ActiveSupport::TestCase
+  test "should have the necessary required validators" do
+    counter = Counter.new
+    counter.value = 0
+
+    counter.increment!
+
+    assert counter.value == 1
+  end
+end
